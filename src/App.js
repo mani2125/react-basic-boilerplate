062ae5c64e1d80ad0fb0645c25050dd7834438bb
mani2125 <manikanda39@gmail.com>
2022-12-12 20:57:32 +0530

    OJ commerce initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./component/header/header";
+
+function App() {
+  return (
+    <div className="ojc-app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/header/header.jsx
@@ -0,0 +1,11 @@
+import './style.scss';
+
+function Header() {
+  return (
+    <header className='header-main'>
+      <span className='header-main__title'>test</span>
+    </header>
+  );
+}
+
+export default Header;
