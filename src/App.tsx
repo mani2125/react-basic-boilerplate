ad072848b8ec2b55c4def8f3a41bf457da45ef30
sureshkarunanidhi <125441787+sureshkarunanidhi@users.noreply.github.com>
2023-04-15 14:50:57 +0530

    SASS integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { Counter } from './Counter'
+import './app.scss'
 
 export const App = () => {
   return (
-    <>
+    <div>
       <h1>React TypeScript Webpack Starter Template</h1>
-      <Counter />
-    </>
+    </div>
   )
 }
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.jsx','.tsx', '.ts', '.js'],
   },
   module: {
     rules: [
@@ -18,8 +18,8 @@ module.exports = {
         ],
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.(s(a|c)ss)$/,
+        use: ['style-loader','css-loader', 'sass-loader']
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
